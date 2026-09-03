49dbb81fe86cb572f774a526a5d51b0a4af9d874
Kevin-alcaino <wthblck1@gmail.com>
2026-09-03 01:46:56 -0400

    se agrega los link de bootstrap

---
+++ src/componets/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><a href="#">🍕Inicio</a></li>
+        <li><a href="#">Ingresar</a></li>
+        <li><a href="#"></a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
